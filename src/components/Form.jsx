bf54e7dbfefd30b61feccc28d4e9158f2300831f
dulbadrah <dolbadrahchuka@gmail.com>
2025-05-11 23:33:32 +0800

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,85 +1,43 @@
 import { useState } from "react";
-export const Form = ({ setTaskList }) => {
+
+
+export const Form = ({ setTaskList, taskList }) => {
   const [inputValue, setInputValue] = useState("");
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
   };
-  console.log(inputValue);
+
   const handleAdd = () => {
     const newTask = {
       id: new Date(),
       taskName: inputValue,
       isCompleted: false,
     };
-    setTaskList((prevTaskList) => [...prevTaskList, newTask]);
+
+    setTaskList([...taskList, newTask]);
     setInputValue("");
   };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "40px",
-      }}
-    >
-      <h1>To-Do List</h1>
-      <div style={{ display: "flex", gap: "20px" }}>
+    <div style={{ display: "flex",
+      gap: "6px"}}>
+      <div>
         <input
+        style={{width: "248px",
+          padding: "12px 16px",
+        borderRadius:"10px"}}
           value={inputValue}
-          style={{
-            borderRadius: "6px",
-            width: "180px",
-          }}
-          placeholder="Add a new task"
+          placeholder="Add a new task..."
+      
           onChange={handleChange}
         />
-        <button
-          style={{
-            backgroundColor: "blue",
-            color: "white",
-            borderRadius: "6px",
-            width: "60px",
-            height: "30px",
-          }}
-          onClick={handleAdd}
-        >
-          add
+      </div>
+      <div>       
+        <button style={{ padding:" 12px 16px",borderRadius:"10px",backgroundColor:"blue",color:"white"}} onClick={handleAdd}>
+          Add
         </button>
       </div>
-      <div
-        style={{
-          display: "flex",
-          gap: "10px",
-        }}
-      >
-        <button
-          style={{
-            backgroundColor: "blue",
-            color: "white",
-            width: "60px",
-            height: "30px",
-            borderRadius: "6px",
-          }}
-        >
-          All
-        </button>
-        <button
-          style={{
-            borderRadius: "6px",
-          }}
-        >
-          Active
-        </button>
-        <button
-          style={{
-            borderRadius: "6px",
-          }}
-        >
-          Completed
-        </button>
-      </div>
-      <div></div>
     </div>
   );
 };
