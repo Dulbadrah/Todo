f29c10cd450bdfdd93912e1b1820e112434956b9
dulbadrah <dolbadrahchuka@gmail.com>
2025-05-11 23:35:14 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,74 @@
-import { Task } from "@/components/Task";
+import { Filter } from "@/components/Filter";
 import { Form } from "@/components/Form";
+import { Task } from "@/components/Task";
 import { useState } from "react";
 
 export default function Home() {
   const [taskList, setTaskList] = useState([]);
-  const removeTaskById = () => {};
+  const [filter, setFilter] = useState("all");
 
-  const ToggleCheckBox = (id) => {
+  const handleDelete = (id) => {
+  setTaskList((prevTasks) => prevTasks.filter((task) => task.id !== id));
+};
+
+
+  const toggleCheckBox = (id) => {
     const toggledTask = taskList.map((task) =>
-      task.id === id
-        ? { ...task, isCompleted: !task.isCompleted }
-        : task.isCompleted
+      task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
     );
+
     setTaskList(toggledTask);
   };
+
+  const filteredTask = taskList.filter((task) => {
+    if (filter === "active" && task.isCompleted === false) {
+      return task;
+    }
+    if (filter === "completed" && task.isCompleted === true) {
+      return task;
+    }
+    if (filter === "all") {
+      return task;
+    }
+  });
+
   return (
+  
     <div
       style={{
         display: "flex",
+        padding:"20px",
         justifyContent: "center",
-        border: "solid 1px black",
-        width: "377px",
-        height: "462px",
-        borderRadius: "40px",
         alignItems: "center",
         flexDirection: "column",
+        maxWidth: "345px",
+        width: "100%",
+        margin: "auto",
+        gap: "10px",
+        border:"solid 1px black",
+        borderRadius:"30px"
       }}
     >
+        
+      <h1>Todo List</h1>
+
       <Form setTaskList={setTaskList} taskList={taskList} />
-      {taskList.map((task) => (
-        <Task
+
+      <Filter filter={filter} setFilter={setFilter} />
+
+
+      {filteredTask.map((task, id) => (
+        <Task 
+          key={id}
           task={task}
-          removeTaskById={removeTaskById}
-          ToggleCheckBox={ToggleCheckBox}
+          removeTaskById={handleDelete}
+          toggleCheckBox={toggleCheckBox}
+          
         />
       ))}
-      <div style={{ display: "flex", gap: "5px" }}>
-        <div>Powered by</div>
-        <a href="">Pinecone academy</a>
+      <div style={{display:"flex",gap:"10px"}}>
+        <p>Poowered By</p>
+         <a href="" style={{paddingTop:"15px"}}>Pinecone academy</a>
       </div>
     </div>
   );
